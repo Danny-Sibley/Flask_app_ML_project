433c48a7d600d8239820751716177fac866b6940
Danny-Sibley <95097205+Danny-Sibley@users.noreply.github.com>
2022-03-13 17:06:07 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+
+from flask import Flask, render_template, redirect, url_for, request, jsonify
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+# deserialize pickle object
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # inputted values from user
+    user_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(user_features)]
+    # creates prediciton using model
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+    # outputs to html file
+    # ZIPCODE, BEDS, BATHS, SQFT, LOT_SIZE, YEAR_BUILT
+    return render_template('index.html', prediction_text=(f'Predicted Home Value: ${output}'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
